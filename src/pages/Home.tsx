557b794ec286865c8ece47c4a472b92fa877d57a
Henry Agu <aguhenrychuks@gmail.com>
2024-03-28 10:15:56 +0100

    FEAT: header updated

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="w-11/12 lg:w-10/12 mx-auto my-5 lg:my-20 font-body">
+      <div>
+        <h1 className="font-semibold text-2xl lg:text-3xl">DCA CALCULATOR</h1>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Home;
