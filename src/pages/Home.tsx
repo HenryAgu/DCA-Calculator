8caf1ab871c1e7d170b7ec779412bb994684647a
Henry Agu <aguhenrychuks@gmail.com>
2024-03-28 10:57:30 +0100

    Feat: transaction updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 const Home = () => {
+  const transaction = [{}, {}];
   return (
     <div className="w-11/12 lg:w-10/12 mx-auto my-5 lg:my-20">
       {/* Header */}
@@ -12,40 +13,42 @@ const Home = () => {
         <button className="bg-primary font-mullish py-3 px-4 rounded-lg text-white">
           Add Transaction
         </button>
-        <div className="mt-10 flex items-baseline gap-x-8">
-          <div className="flex items-center gap-x-2 font-redHatDisplay text-grey-100 font-semibold lg:text-2xl">
-            <p>1.</p>
-            <p>Transaction 01</p>
-          </div>
-          <div className="flex items-center gap-x-8">
-            <div className="flex flex-col gap-y-1">
-              <input
-                type="text"
-                className="p-3 rounded-lg border border-purple-50"
-              />
+        {transaction.map((transaction, index) => (
+          <div className="mt-10 flex flex-col lg:flex-row lg:items-end gap-x-8">
+            <div className="flex items-center gap-x-2 font-redHatDisplay text-grey-100 font-semibold lg:text-2xl">
+              <p>{index + 1}.</p>
+              <p>Transaction 0{index + 1}</p>
+            </div>
+            <div className="flex flex-col lg:flex-row gap-y-5 items-center gap-x-8">
+              <div className="flex flex-col gap-y-1">
+                <input
+                  type="text"
+                  className="p-3 w-[90vw] lg:w-[100%] lg:p-3 rounded-lg border border-purple-50"
+                />
+                <span className="text-grey-100 text-sm font-bold">
+                  Total Spent on Transaction
+                </span>
+              </div>
+              <div className="flex flex-col gap-y-1">
+                <input
+                  type="text"
+                  className="p-3 w-[90vw] lg:w-[100%] lg:p-3 rounded-lg border border-purple-50"
+                />
+                <span className="text-grey-100 text-sm font-bold">
+                  Cost of stock
+                </span>
+              </div>
+            </div>
+            <div className="ml-22 mt-6">
+              <p className="p-3 rounded-lg border border-purple-50 shadow-lg font-redHatDisplay">
+                0
+              </p>
               <span className="text-grey-100 text-sm font-bold">
-                Total Spent on Transaction
+                Units Recieved
               </span>
             </div>
-            <div className="flex flex-col gap-y-1">
-              <input
-                type="text"
-                className="p-3 rounded-lg border border-purple-50"
-              />
-              <span className="text-grey-100 text-sm font-bold">
-                Cost of stock
-              </span>
-            </div>
           </div>
-          <div className="ml-22">
-            <p className="p-3 rounded-lg border border-purple-50 shadow-lg">
-              0
-            </p>
-            <span className="text-grey-100 text-sm font-bold">
-              Units Recieved
-            </span>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
