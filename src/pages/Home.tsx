eb06e336588580f06f8cb84fabe92257896d953b
Henry Agu <aguhenrychuks@gmail.com>
2024-03-30 18:31:43 +0100

    feat: delete transaction

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,10 +13,17 @@ const Home = () => {
     const newTransaction = {
       id: transactions.length + 1,
     };
-    setTransactions([...transactions, newTransaction])
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  const handleDeleteTransaction = (id:number) => {
+    const updatedTransaction = transactions.filter(
+      (transaction) => transaction.id !== id
+    );
+    setTransactions(updatedTransaction);
   };
   return (
-    <div className="relative flex flex-col w-11/12 lg:w-10/12 mx-auto my-5 lg:my-20">
+    <div className="relative flex flex-col w-11/12 mx-auto my-5 lg:my-20">
       {/* Header */}
       <div>
         <h1 className="font-redHatDisplay font-semibold text-2xl lg:text-3xl">
@@ -32,7 +39,15 @@ const Home = () => {
           Add Transaction
         </button>
         {transactions.map((transaction) => (
-          <Transaction transaction={transaction} />
+          <div className="flex flex-col md:flex-row md:items-center">
+            <Transaction transaction={transaction} />
+            <button
+              className="md:ml-2 mt-5 md:mt-10 bg-red-500 text-white px-3 py-1 rounded w-fit"
+              onClick={() => handleDeleteTransaction(transaction.id)}
+            >
+              Delete
+            </button>
+          </div>
         ))}
       </div>
 
